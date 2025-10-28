2e93ed960cd47071a741f975ce5d699d21ddec0c
RLuk97 <ryan.prog97@gmail.com>
2025-10-27 22:41:33 -0400

    chore(footer): usar imagem PNG da Fametro no rodapé

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,9 +4,9 @@ export default function Footer() {
       <div className="relative mx-auto max-w-5xl px-6 py-6 text-center text-sm text-blue-700">
         {/* Logo à esquerda, mantendo o texto centralizado */}
         <img
-          src="/fametro.svg"
+          src="/FAMETRO.png"
           alt="Grupo FAMETRO"
-          className="hidden sm:block absolute left-6 top-1/2 -translate-y-1/2 h-8 w-auto"
+          className="hidden sm:block absolute left-6 top-1/2 -translate-y-1/2 h-8 w-auto object-contain"
         />
         Azul Pela Vida • Novembro Azul • Fontes: INCA, IARC/WHO
       </div>
